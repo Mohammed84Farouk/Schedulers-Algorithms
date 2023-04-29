68edf56041681cbc61bd6a5dc7ac7fa1272294f5
Mohamed Fathy <mofathy622000@gmail.com>
2023-04-29 20:47:15 +0300

    preemptive and non preemptive SJF done!

--- src/main/java/com/schedulers_algorithms/Non_Preemptive_SJF/SJFS.java
+++ src/main/java/com/schedulers_algorithms/Non_Preemptive_SJF/SJFS.java
@@ -18,17 +18,19 @@ public class SJFS implements AlgorithmType {
     private boolean isPreemptive = false;
     private double AverageWaitingTime = 0;
     private double AverageTurnAroundTime = 0;
+    private int nProcess = 0;
     private Process currentProcess;
+
     public SJFS(boolean isPreemptive) {
         this.isPreemptive = isPreemptive;
         this.currentProcess = null;
     }
 
     @Override
-    public void addProcessToReadyQueue(Process process){
+    public void addProcessToReadyQueue(Process process) {
         processesA.add(process);
+        nProcess++;
     }
-
 
 
     @Override
@@ -42,33 +44,59 @@ public class SJFS implements AlgorithmType {
     }
 
     @Override
-    public void executeProcess(){
-        if(currentProcess != null)currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
-        if(currentProcess == null || currentProcess.getBurstTime() == 0){
-            if(!getProcessFromReadyQueue()){
+    public void executeProcess() {
+        if (currentProcess != null) {
+            if (!isPreemptive) currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
+            else {
+                currentProcess.setBurstTime(currentProcess.getBurstTime() - 1);
+                currentProcess.setWaitingTime(currentProcess.getWaitingTime() - 1);
+                if (currentProcess.getBurstTime() != 0) processesA.add(currentProcess);
+                else {
+
+                    currentProcess.setTurnAroundTime(getCurrentTime() - currentProcess.getArrivalTime());
+                    currentProcess.setWaitingTime(currentProcess.getTurnAroundTime() + currentProcess.getWaitingTime());
+                    AverageWaitingTime += currentProcess.getWaitingTime();
+                    AverageTurnAroundTime += currentProcess.getTurnAroundTime();
+                    System.out.println(currentProcess.getId() + currentProcess.getWaitingTime());
+                    System.out.println(currentProcess.getId() + currentProcess.getTurnAroundTime());
+                }
+                currentProcess = null;
+            }
+        }
+        if (currentProcess == null || currentProcess.getBurstTime() == 0) {
+            if (!getProcessFromReadyQueue()) {
                 currentProcess = null;
             }
         }
 
     }
-    private boolean getProcessFromReadyQueue(){
+
+    private boolean getProcessFromReadyQueue() {
         int currentTime = getCurrentTime();
-        System.out.println(currentTime);
-        while(!processesA.isEmpty() && currentTime >= processesA.first().getArrivalTime()){
+        while (!processesA.isEmpty() && currentTime >= processesA.first().getArrivalTime()) {
             processesB.add(processesA.first());
             processesA.remove(processesA.first());
         }
-        if(processesB.isEmpty())  {
-            return false;
+        if (processesB.isEmpty() && processesA.isEmpty()) {
+            System.out.println(AverageWaitingTime / nProcess);
+            System.out.println(AverageTurnAroundTime / nProcess);
+            System.out.println(nProcess);
         }
-        else currentProcess = processesB.first();
-        currentProcess.setWaitingTime(currentTime-currentProcess.getArrivalTime());
-        currentProcess.setTurnAroundTime(currentTime - currentProcess.getArrivalTime());
-        this.AverageWaitingTime+= currentProcess.getWaitingTime();
-        this.AverageTurnAroundTime+=(currentTime - currentProcess.getArrivalTime());
-        processesB.remove(currentProcess);
-        return true;
+        if (processesB.isEmpty()) {
+            return false;
+        } else currentProcess = processesB.first();
+
+        //Logic according to the type of SJF scheduler
+        if (!isPreemptive) {
+            currentProcess.setWaitingTime(currentTime - currentProcess.getArrivalTime());
+            currentProcess.setTurnAroundTime(currentTime - currentProcess.getArrivalTime());
+            this.AverageWaitingTime += currentProcess.getWaitingTime();
+            this.AverageTurnAroundTime += (currentTime - currentProcess.getArrivalTime());
+            processesB.remove(currentProcess);
+            return true;
+        } else {
+            processesB.remove(currentProcess);
+            return true;
+        }
     }
-
-
 }
